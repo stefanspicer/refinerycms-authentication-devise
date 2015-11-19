60058ecf58c38a1c5bd1d23b51d7b2101ec15e4f
Stefan <stefan@glasscanvas.io>
2015-11-18 21:08:27 -0800

    Revert "Add I18n for :en and :fr (dropped other locales)"
    This reverts commit 872e4d24a99a9d45715cbe29f717526e18f4bbdf.
    
    Conflicts:
    	app/views/refinery/authentication/devise/admin/users/_form.html.erb

--- app/mailers/devise/glass_mailer.rb
+++ app/mailers/devise/glass_mailer.rb
@@ -3,7 +3,7 @@ class Devise::GlassMailer < Devise::Mailer
     # Set a custom subject for an email that goes out as
     # an invitation email.
     unless record.confirmed?
-      opts[:subject] = t('welcome_message', :scope => 'refinery.authentication.devise.user_mailer.invitation_email', :site_name => Refinery::Core.site_name)
+      opts[:subject] = "Welcome to #{Refinery::Core.site_name}"
       @admin_url = refinery.admin_root_url
       @site_url  = refinery.root_url
     end
